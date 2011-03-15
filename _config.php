7939328876040ffa1de0216c58b4e715a8288084
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-03-15 01:02:06 +0000

    adding _config, LICENSE, and README to ecommerce_tax
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_tax/trunk@953 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+ * developed by www.sunnysideup.co.nz
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+**/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerce_tax MODULE ----------------===================
+//===================---------------- END ecommerce_tax MODULE ----------------===================
