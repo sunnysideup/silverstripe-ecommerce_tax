3983968aabe0851f38bc31932a444cb612184acf
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-09-20 05:12:53 +0000

    cleaned up config file
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_tax/trunk@1495 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -10,7 +10,11 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START ecommerce_tax MODULE ----------------===================
 // *** GST TAX MODIFIER
+//MUST SET
 //Order::add_modifier("GSTTaxModifier");
+//HIGHLY RECOMMENDED
+//StoreAdmin::add_managed_model("GSTTaxModifierOptions");
+//MAY SET
 //GSTTaxModifier::set_default_country_code("NZ");
 //GSTTaxModifier::set_fixed_country_code("NZ");
 //GSTTaxModifier::set_exclusive_explanation(" (to be added to prices above)");
@@ -19,5 +23,4 @@
 //GSTTaxModifier::set_no_tax_description("tax-exempt");
 //GSTTaxModifier::set_refund_title("Tax Exemption");
 //GSTTaxModifier::set_order_item_function_for_tax_exclusive_portion("PortionWithoutTax");
-//StoreAdmin::add_managed_model("GSTTaxModifierOptions");
 //===================---------------- END ecommerce_tax MODULE ----------------===================
