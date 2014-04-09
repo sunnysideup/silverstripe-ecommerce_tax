c1c66249052dcc3ac5259c5075449a74abb94c2a
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:40:16 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_tax/trunk@4928 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -6,8 +6,3 @@
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
 
-
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-
-//===================---------------- START ecommerce_tax MODULE ----------------=================== 
-//===================---------------- END ecommerce_tax MODULE ----------------===================
